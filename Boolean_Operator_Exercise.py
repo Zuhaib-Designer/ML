bbc7cc712af2d1e43896640a7a9342401b18d849
Zohaib Hassan <141019974+Zuhaib-Designer@users.noreply.github.com>
2026-03-14 12:03:17 +0000

    Added Python Exercises

---
+++ Boolean_Operator_Exercise.py
@@ -0,0 +1,7 @@
+Status_of_Student = input("Are You Student")
+
+student = "Yes" or "Y" or "yes" or "y"
+Have_pets = True
+Have_smoke = True
+if student == Status_of_Student:
+    if 
---
+++ if.py
@@ -0,0 +1,17 @@
+Worker1 = "John"
+Worker2 = "Rachle"
+Professor = "Falcon"
+
+name = input("Enter Your name.")
+
+if name == Worker1:
+    print("Hello John!")
+elif name == Worker2:
+    print("Hello Rachle!")
+elif name == Professor:
+    print("Greetings Professor Falcon!")
+
+print("Program Finished")
+
+print("cat">"Dog")
+print("cat"<"Dog")
